cef5928c46cef6147e33e77a03a36d01a155bc1c
Joey Chen <jay.hatelove@gmail.com>
2019-10-18 16:33:35 +0800

    delete unuse reference

--- IsolatedByInheritanceAndOverride/OrderService.cs
+++ IsolatedByInheritanceAndOverride/OrderService.cs
@@ -72,11 +72,8 @@ namespace IsolatedByInheritanceAndOverride
     public class Order
     {
         public string Type { get; set; }
-
         public int Price { get; set; }
-
         public string ProductName { get; set; }
-
         public string CustomerName { get; set; }
     }
 
